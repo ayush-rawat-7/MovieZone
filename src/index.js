6cf9a9cb0ed5587dbb2e148c47041971c05ed9ab
ayush-rawat-7 <rwat905@gmail.com>
2021-11-20 18:23:36 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri="https://movie-zone-v1.netlify.app/home"
+      redirectUri="https://moviezonewebapp.netlify.app/home"
       cacheLocation="localstorage"
     >
       <MoviesProvider>
